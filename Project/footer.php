41128e2a59ca5d178538851f919cf74ba81ad452
edaniels20 <ethandanielscoding@gmail.com>
2021-08-11 11:33:38 -0500

    removed llc from footer

--- Project/footer.php
+++ Project/footer.php
@@ -9,7 +9,7 @@
 <div class="container footer-outer">
     <div class="row">
         <div class="col-12">
-            <span>Ethan Pizza &copy; 2018 Ethan Daniels LLC</span>
+            <span>Ethan Pizza &copy; 2018 Ethan Daniels</span>
         </div>
     </div>
 </div>
